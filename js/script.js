57aa2f11a48bc0ab62c3567ac2fb062216fee874
Ajaykaran Singh Grewal <aksgrewal@gmail.com>
2017-08-24 02:27:29 -0400

    first-commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+   /*$('button').click(function(){
+       $('button').removeClass('selected');
+       $(this).addClass("selected");*/
+    
+    $('form').submit(function(event){
+        event.preventDefault();
+        var $searchField = $('#inputText');
+       
+       var apiString = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+       var animal = $searchField.val();
+       
+       var flickerOptions = {
+           tags: animal,
+           format: "json"
+       };
+       
+       function displayPhotos(dataArray){
+           var photoHTML = '<div class="row text-center text-lg-left">';
+           
+           $.each(dataArray.items, function(i, photo){
+              photoHTML += '<div class="col-lg-3 col-md-4 col-xs-6">';
+              photoHTML += '<a href = "' +photo.link+ '" class = "d-block mb-4 h-100">';
+              photoHTML += '<img src="' +photo.media.m+ '" class="img-fluid img-thumbnail"></a></div>';
+           });
+           photoHTML += '</div>'
+           $('#photos').html(photoHTML);
+       }
+       $.getJSON(apiString, flickerOptions, displayPhotos);
+   }); 
+    
+    var $overlay = $('<div id="overlay"></div>');
+    var $image = $('<img>');
+    
+    $overlay.append($image);
+    $body.append($overlay);
+    
+    $('#photos a').click(function(event){
+        event.preventDefault();
+        
+        var imageLocation = $(this).attr('href');
+        $image.attr('src', imageLocation);
+        
+        $overlay.show();
+    });
+    
+    $overlay.click(function(){
+       $overlay.hide(); 
+    });
+});

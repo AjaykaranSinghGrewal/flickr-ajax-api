3f1455eb6bd5246b0695932071d2a8e4c7d7ff2d
Ajaykaran Singh Grewal <aksgrewal@gmail.com>
2017-08-24 03:49:32 -0400

    second-commit

--- js/script.js
+++ js/script.js
@@ -29,22 +29,4 @@ $(document).ready(function(){
        $.getJSON(apiString, flickerOptions, displayPhotos);
    }); 
     
-    var $overlay = $('<div id="overlay"></div>');
-    var $image = $('<img>');
-    
-    $overlay.append($image);
-    $body.append($overlay);
-    
-    $('#photos a').click(function(event){
-        event.preventDefault();
-        
-        var imageLocation = $(this).attr('href');
-        $image.attr('src', imageLocation);
-        
-        $overlay.show();
-    });
-    
-    $overlay.click(function(){
-       $overlay.hide(); 
-    });
 });
